c83b3120f2c9636c2fc145aab96e2d08215ef677
ozgeceleb <celebiiozgee@gmail.com>
2023-07-11 17:43:33 +0300

    if-else

---
+++ conditions.js
@@ -0,0 +1,21 @@
+/*let question = prompt ("What is the 2 +5 ?")
+
+if (question== 7){
+    console.log('congratulations')
+} else 
+  console.log('The answer will be 7')
+
+*/
+
+let myAge =prompt ("What is your age ? :")
+let yourAge = prompt('What is your his/her age ? :')
+
+if(myAge>yourAge){
+
+    let remainder = myAge-yourAge
+    console.log('you are ' + remainder+ ' years older than his')
+} else (yourAge>myAge)
+    let remainder2 = yourAge-myAge
+    console.log('You are'+ remainder2+ ' years younger than his' ) 
+
+
---
+++ if-else.js
@@ -0,0 +1,9 @@
+let number = prompt('Enter an number :' )
+
+
+if (even%2==1) {
+  console.log('This is odd number')
+
+}else{
+console.log('This is odd number')
+}            
