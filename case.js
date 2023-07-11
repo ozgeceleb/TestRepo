721c97fd082c71a9f325777309b420b2a8cbebbd
ozgeceleb <celebiiozgee@gmail.com>
2023-07-11 18:03:14 +0300

    if-else2

---
+++ case.js
@@ -0,0 +1,26 @@
+// Write a code which can give grades to students according to theirs scores
+
+let notes = prompt ('What is the note ?')
+
+switch(notes) {
+    case '80':
+     console.log('A')
+
+     break
+     case '79':
+     console.log('B')
+     break
+     case '69':
+     console.log('C')
+     break
+
+     case '59':
+     console.log('D')
+     break
+     
+     case '49':
+     console.log('F')
+     break   
+
+}
+     
---
+++ if-else2.js
@@ -0,0 +1,12 @@
+let notes = prompt('What is the note ?')
+
+if (notes >=80) {
+    console.log('A')
+}else if (notes>=70){
+    console.log('B')
+}else if(notes>=60){
+    console.log('C')
+}else if(notes>=50){
+    console.log('D')
+}else
+    console.log('F')
